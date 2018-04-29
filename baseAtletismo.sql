979a239cac8615a86b1207140b97b82c0f9c36ae
Cake <melissaqgaspar@gmail.com>
2018-04-29 16:07:48 -0300

    Query atualizada

--- baseAtletismo.sql
+++ baseAtletismo.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE atletismo
-USE atletismo
 GO
+USE atletismo
+
 
 CREATE TABLE pais(
 	cod VARCHAR(3) NOT NULL,
@@ -8,10 +9,36 @@ CREATE TABLE pais(
 	PRIMARY KEY(cod)
 )
 CREATE TABLE atleta(
-	cod INT NOT NULL,
+	cod INT IDENTITY NOT NULL,
 	cod_pais VARCHAR(3),
 	nome VARCHAR (100),
 	sexo VARCHAR(1),
 	PRIMARY KEY(cod),
 	FOREIGN KEY (cod_pais) REFERENCES pais(cod)
+)
+CREATE TABLE prova(
+cod INT IDENTITY NOT NULL,
+prova VARCHAR(50) NOT NULL,
+sexo varchar(1) NOT NULL,
+tipo INT NOT NULL,
+PRIMARY KEY (cod)
+)
+
+
+CREATE TABLE prova_atleta(
+cod_prova INT NOT NULL,
+cod_atleta INT NOT NULL,
+resultado DECIMAL (7, 2),
+PRIMARY KEY(cod_prova, cod_atleta),
+FOREIGN KEY (cod_prova) REFERENCES prova(cod),
+FOREIGN KEY (cod_atleta) REFERENCES atleta(cod)
+)
+
+CREATE TABLE recorde(
+cod_prova INT NOT NULL,
+cod_atleta INT NOT NULL,
+resultado DECIMAL (7, 2),
+PRIMARY KEY (cod_prova, cod_atleta),
+FOREIGN KEY (cod_prova) REFERENCES prova(cod),
+FOREIGN KEY (cod_atleta) REFERENCES atleta(cod)
 )
